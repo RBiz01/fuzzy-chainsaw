134a686395a3908cb42dabd54a558d8a904ecd34
RBiz01 <95893992+RBiz01@users.noreply.github.com>
2026-03-09 20:01:50 -0500

    Create level.js

---
+++ tanky_assets/level.js
@@ -0,0 +1,18 @@
+[
+  {
+    "enemies": [{"x":180,"y":-200}],
+    "theme": "flat",
+    "baseScore": 100
+  },
+  {
+    "enemies": [{"x":220,"y":-200}, {"x":320,"y":-200}],
+    "theme": "flat",
+    "baseScore": 150
+  },
+  {
+    "enemies": [{"x":180,"y":-200}, {"x":320,"y":-280}, {"x":420,"y":-200}],
+    "theme": "flat",
+    "baseScore": 200
+  },
+ 
+]
